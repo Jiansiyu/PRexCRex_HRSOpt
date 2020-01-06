3587e9b7e8221d6dffc570d8984ab2bee4551771
Siyu <jiansiyu@gmail.com>
2020-01-06 17:43:18 -0500

    add some datafile

--- Opt/ROpticsOptScript.C
+++ Opt/ROpticsOptScript.C
@@ -30,8 +30,8 @@ Bool_t freepara[10000] = {kFALSE}; //NPara
 UInt_t MaxDataPerGroup = 100;
 
 //TString DataSource = "/home/newdriver/Research/Eclipse_Workspace/photonSep2019/PRexOpt/asciReform/SieveReform/Sieve.Full.test";
-TString DataSource = "/home/newdriver/Research/Eclipse_Workspace/photonSep2019/PRexOpt/asciReform/SieveReform/temp.dat";
 //TString DataSource = "/home/newdriver/Storage/Research/Eclipse_Workspace/photonSep2019/PRexOpt/asciReform/DpReform/temp.dat";
+TString DataSource = "/home/newdriver/Research/Eclipse_Workspace/photonSep2019/PRexOpt/asciReform/SieveReform/Sieve._2565_2550_2565_p4.f51_reform";
 
 typedef void (*PTRFCN)(Int_t &, Double_t *, Double_t &, Double_t*, Int_t);
 PTRFCN myfcn = NULL;
--- asciReform/SieveReform/trans.C
+++ asciReform/SieveReform/trans.C
@@ -1,78 +1,55 @@
+/*
+ *
+ *
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
+
+inline std::string getFileName(const std::string & s){
+	char sep = '/';
+#ifdef _WIN32
+        sep = '\\';
+#endif
+	size_t i = s.rfind(sep, s.length());
+	if(i != std::string::npos){
+	    return(s.substr(i+1, s.length()-i));
+	}  
+        return("");
+}
+
 int main(int argc, char* argv[])
 {
 	FILE *fp1,*fp2;
-	//char newname[100];
-	//sprintf(newname,"%s.new",argv[1]);
-	fp2=fopen("temp.dat","w");
-    std::ifstream file(argv[1]);
-    std::string line;
-    while(std::getline(file, line)){
-        unsigned int index=0;
-        double d[20];
-
-        std::stringstream linestream(line.c_str());
-                              //kx     kth     ky     kphi     kurb_e  kbeamx   kbeamy
-        linestream >> index >> d[0] >> d[1] >> d[2] >> d[3] >> d[4] >> d[5] >> d[6];
-        
-/*        const unsigned int NSieveCol = 13;
-        const unsigned int NSieveRow = 7;
-        auto  DpID = (unsigned int)index / (NSieveRow * (NSieveCol+1));//starting 0!
+	char newname[200];
+	sprintf(newname,"%s_reform",getFileName(argv[1]).c_str());
 	
-	unsigned int res=(unsigned int)index % (NSieveRow * (NSieveCol+1));
-	 unsigned int Col = res / (NSieveRow); //starting 0!
-	 unsigned int Row = res % (NSieveRow); //starting 0!
-	 unsigned int FoilID=0;
-        
-        index=(Col<<16)|(Row);
-        std::cout <<"Col:: "<<Col<<"    Shift::"<<(Col<<16)<<"  Combined : "<<((Col<<16)|(Row))<<"   Row:"<<Row<< "  Combined Index:"<<index<<std::endl;*/
-        fprintf(fp2,"%d\t%1.8e\t%1.8e\t%1.8e\t%1.8e\t%1.8e\t%1.8e\t%1.8e\t%1.8e\n",index,d[0],d[1],d[2],d[3],2.17615,d[5]/1000.0,d[6]/1000.0,0.0);
-    }
-    fclose(fp2);
+	fp2=fopen(newname,"w");
+    	std::ifstream file(argv[1]);
+    	std::string line;
+    	
+    	while(std::getline(file, line)){
+        	unsigned int index=0;
+        	double d[20];
 
-    std::cout << '\a' << std::flush;
-    
-    /*
-    // encode and decode test for the new
-    
-    for (unsigned int MomID = 0 ; MomID < 15 ; MomID++){
-          
-       for (unsigned int FoilID =0 ; FoilID < 15 ; FoilID ++){
-       
-            for (unsigned int ColID = 0; ColID<15; ColID++){
-            
-                for (unsigned int RowID=0; RowID<15; RowID++){
-                	auto mom=MomID;
-                	auto foil=FoilID;
-                	auto Col=ColID;
-                	auto Row=RowID;
-                	
-                	unsigned int encoded=(mom<<48)|(foil<<32)|(Col<<16)|(Row);
-                	unsigned int temp=encoded;
-                	unsigned int decodedRow=(encoded)&(0xffff);
-                	unsigned int decodedCol=(encoded>>16)&(0xffff);
-                	unsigned int decodedfoil=(encoded>>16)&(0xffff);
-                	unsigned int decodedmom=(encoded>>16)&(0xffff);
-                	
-                	std::cout<< "original ("<< MomID<<", "<< FoilID<<", "<<ColID<<", "<<RowID<<")   encode to :"  <<encoded;
-                	std::cout<< "  ==> decode ("<< mom<<", "<< foil<<", "<<Col<<", "<<Row<<")";
-                	
-                	if ((MomID==mom) && (FoilID==foil) && (ColID==Col) && (RowID==Row)){
-                	std::cout<<"   Pass"<<std::endl;
-                	
-                	} 
+        	std::stringstream linestream(line.c_str());
 
-                     
-                }
-            
-            }
-       }
-    }
-    */
-    return 0;
+                              //kx  kth      ky     kphi    kurb_e  kbeamx  kbeamy   kbeam_z
+        	linestream >> index >> d[0] >> d[1] >> d[2] >> d[3] >> d[4] >> d[5] >> d[6];
+        									         //kx     kth      ky     kphi    kurb_e  kbeamx  kbeamy   kbeam_z
+        	fprintf(fp2,"%d\t%1.8e\t%1.8e\t%1.8e\t%1.8e\t%1.8e\t%1.8e\t%1.8e\t%1.8e\n",index,d[0],d[1],d[2],d[3],2.17615,d[5]/1000.0,d[6]/1000.0,0.0);
+
+    	}
+
+    	fclose(fp2);
+
+    	std::cout << '\a' << std::flush;
+    	std::cout<<"reformed file saved as :"<<newname<<std::endl;
+    	
+    	return 0;
 }
