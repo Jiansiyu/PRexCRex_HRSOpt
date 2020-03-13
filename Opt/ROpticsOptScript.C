75ae0f9f17108888475aa8f92e55c2b4f5b67feb
Siyu <jiansiyu@gmail.com>
2020-03-13 00:21:57 -0400

    change to 8nknie with issues

--- Opt/ROpticsOptScript.C
+++ Opt/ROpticsOptScript.C
@@ -13,7 +13,7 @@
 
 #define th_ph_optimize false
 #define y_optimize false
-#define dp_optimize true
+#define dp_optimize false
 
 #include "ROpticsOpt.h"
 //#include "SaveCanvas.C"
@@ -29,7 +29,8 @@ Bool_t freepara[10000] = {kFALSE}; //NPara
 
 UInt_t MaxDataPerGroup = 100;
 
-TString DataSource = "/home/newdriver/Research/Eclipse_Workspace/photonSep2019/PRexOpt/asciReform/SieveReform/Sieve.Full.test_reform";
+//TString DataSource = "/home/newdriver/Research/Eclipse_Workspace/photonSep2019/PRexOpt/asciReform/SieveReform/Sieve.Full.test_reform";
+TString DataSource = "/home/newdriver/Research/Eclipse_Workspace/photonSep2019/PRexOpt/asciReform/SieveReform/SieveThetaPhi.Full.test_reform";
 
 
 typedef void (*PTRFCN)(Int_t &, Double_t *, Double_t &, Double_t*, Int_t);
