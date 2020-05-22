0cedede4fa26af3382faf5018abbbcdd6340cbc9
Siyu <jiansiyu@gmail.com>
2020-05-22 16:11:36 -0400

    add the optscan code

---
+++ OptScan/script/getOptProgress.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+FolderPath=$1
+
+
+pstr="[=======================================================================]"
+
+TotalFileNumber=$(ls ${FolderPath} | wc -l)
+echo "Total Files need to Process : ${TotalFileNumber}"
+echo "Current progress :"
+
+CurrentReplayedNumber=$(ls ${FolderPath}/*/CheckDp_test_result.txt | wc -l) 
+
+while [ ${TotalFileNumber} -gt ${CurrentReplayedNumber} ];
+do 
+	
+	pd=$(( ${CurrentReplayedNumber} * 73 / ${TotalFileNumber} ))
+	printf "\r%3d.%1d%% %.${pd}s"  $(( ${CurrentReplayedNumber} * 100 / ${TotalFileNumber} )) $(( (${CurrentReplayedNumber} * 10000 / ${TotalFileNumber}) % 100 ))  $pstr
+	sleep 20
+	CurrentReplayedNumber=$(ls ${FolderPath}/*/CheckDp_test_result.txt | wc -l)
+done 
+
+# post run process
+
+CurrentReplayedNumber=$(ls ${FolderPath}/*/CheckDp_test_result.txt | wc -l)
+pd=$(( ${CurrentReplayedNumber} * 73 / ${TotalFileNumber} ))
+printf "\r%3d.%1d%% %.${pd}s"  $(( ${CurrentReplayedNumber} * 100 / ${TotalFileNumber} )) $(( (${CurrentReplayedNumber} * 10000 / ${TotalFileNumber}) % 100 ))  $pstr
+
+echo 
+echo "Process Done !!!"
