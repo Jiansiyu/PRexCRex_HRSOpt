c434fa08051cf842d297633278daa9d98981331a
Siyu <jiansiyu@gmail.com>
2021-05-25 14:19:07 -0400

    change the scan files

--- OptScan_PRex_2021/run.sh
+++ OptScan_PRex_2021/run.sh
@@ -1,7 +1,7 @@
 source venv/bin/activate
 # rm /home/newdriver/Storage/Research/Eclipse_Workspace/photonSep2019/PRexOpt/OptScan_Prex/PRex_LHRS_result/LHRS_scan_phi_allrun_rescan/* -r
 # rm /home/newdriver/Storage/Research/Eclipse_Workspace/photonSep2019/PRexOpt/OptScan_Prex/PRex_LHRS_result/LHRS_scan_theta_allrun_rescan/* -r
-workDir="/home/newdriver/Storage/Research/Eclipse_Workspace/photonSep2019/PRexOpt/OptScan_PRex_2021/result/LHRS/20210201_scan_Phi_test6/*"
+workDir="/home/newdriver/Storage/Research/Eclipse_Workspace/photonSep2019/PRexOpt/OptScan_PRex_2021/result/LHRS/20210201_scan_Phi_test7/*"
 echo "Will delet all file in folder : ${workDir}"
 read -p "Are you sure re-run Opt? " -n 1 -r
 echo    # (optional) move to a new line
